c06bc784e729242b6ba1259ce15dbfc4099fc956
tatianacardosolima <tatidornel@gmail.com>
2024-04-18 19:19:39 -0300

    Exemplo com MessagePackSerializer

---
+++ src/Fiap.Fase1.NET8/Controllers/MessagePackController.cs
@@ -0,0 +1,47 @@
+﻿using MessagePack;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Fiap.Fase1.NET8.Controllers
+{
+    [ApiController]
+    [Route("messagepackteste")]
+    public class MessagePackController : ControllerBase
+    {
+        public MessagePackController()
+        {
+                
+        }
+        [HttpPost("Serialize-mp")]
+        public IActionResult SerializableJson()
+        {
+            var dados = new DadosMP() { Id = 100, Name = "Joao", IsEnable = false};
+            var json = MessagePackSerializer.Serialize(dados);
+            var ret = MessagePackSerializer.ConvertToJson(json);
+            return Ok(ret);
+        }
+
+        [HttpPost("deserializable-mp")]
+        public IActionResult DeserializableJson()
+        {
+            var dados = new DadosMP() { Id = 100, Name = "Joao", IsEnable = false };
+            var json = MessagePackSerializer.Serialize(dados);
+            var ret = MessagePackSerializer.Deserialize<DadosMP>(json);
+
+
+            return Ok(ret);
+        }
+    }
+    [MessagePackObject]
+    public class DadosMP
+    {
+        [Key(0)]
+        public int Id { get; set; }
+        [Key(1)]
+        public string Name { get; set; }
+        [Key(2)]
+        public bool IsEnable { get; set; } = true;
+    }
+
+
+}
